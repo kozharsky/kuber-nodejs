864709815fbcf3bc026d3ef241a5ebf02516ba72
Andriy Kopachevskiy <support@devunet.com>
2016-12-01 12:55:57 +0200

    created-folder

--- docker-images/push-all.sh
+++ docker-images/push-all.sh
@@ -5,7 +5,7 @@ set -e
 eval $(aws ecr get-login --region ${REGION})
 
 for image in \
-        nodejs \
+        docker-images/nodejs \
     ; do
         ECR_TAG="${ECR_REPO}/${image}:latest"
         docker tag "${image}:latest" "${ECR_TAG}"
